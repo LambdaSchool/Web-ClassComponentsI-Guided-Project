a94a2bec4bb0b1bf83922174acda33314170c122
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-12 16:57:35 +0100

    fix import

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 // Implement Count component so it renders the `count` in an h3,
 // and also two button elements that invoke `increment` and `decrement` on click.
 // Wrap everything in a div with the class of 'sub-container' so styles will work.
-export function Count({ count, increment, decrement }) {
+export default function Count({ count, increment, decrement }) {
   return null;
 }
