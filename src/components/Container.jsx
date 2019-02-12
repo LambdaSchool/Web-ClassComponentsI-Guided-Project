b1b1537c2823807cbbb193913d58ed3d1c6afaaa
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-12 16:53:02 +0100

    better instructions

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Count from './Count';
 import Friends from './Friends';
 
-// 1- Implement Count functional component
-// 2- Implement Friends functional component
-// 3- Refactor Container so it has a constructor
-// 4- Initialize state in constructor with slices `count` and `friends`
-// 5- Have Count and Friends consume the state
+// 1- Implement `Count` functional component
+// 2- Implement `Friends` functional component
+// 3- Refactor `Container` so it has a constructor
+// 4- Initialize state in constructor with fields `count` and `friends`
+// 5- Have `Count` and `Friends` consume the state
 // 6- Create `increment` and `decrement` methods in Container (learn `setState`)
 // 7- Pass methods to Count component
 // 8- Create an `addFriend` method in Container that takes `newFriendName`
@@ -18,7 +18,7 @@ class Container extends React.Component {
     return (
       <div className='container'>
         <Count
-          count={this.state.count}
+          count={7}
           increment={() => console.log('incrementing!')}
           decrement={() => console.log('decrementing!')}
         />
--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-// Implement Friends component so it takes a list of friends,
+// Implement `Friends` component so it takes an array of friends,
 // each friend being an object like so: { name: 'Peter' },
-// and renders the friends' names.
+// and renders a list of friends' names.
 // Wrap everything in a div with the class of 'sub-container' so styles will work.
 export default function Friends({ friends }) {
   return null;
